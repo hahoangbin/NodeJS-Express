fc9626c12f3abd20988e3ff65fc452f6a41bd516
Ha Ngoc Duong <hahoangbin999@gmail.com>
2023-07-20 22:02:55 +0700

    POST method

--- blogger/src/index.js
+++ blogger/src/index.js
@@ -6,6 +6,10 @@ const app = express()
 const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.render('home')
@@ -15,6 +19,10 @@ app.get('/news', (req, res) => {
 })
 app.get('/search', (req, res) => {
   res.render('search')
+})
+app.post('/search', (req, res) => {
+  console.log(req.body);
+  res.send('dfgsdfg')
 })
 
 // HTTP logger
